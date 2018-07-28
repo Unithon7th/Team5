1f5cf5569b6cf1efada276bc4cccf5e829454b93
len <joenggyu0@gmail.com>
2018-07-29 05:46:28 +0900

    chore: HTTP 연결 남음

--- unithon_nb/app/src/main/java/com/unithon7th/unithon_nb/AgriculturalRegisterFragment.java
+++ unithon_nb/app/src/main/java/com/unithon7th/unithon_nb/AgriculturalRegisterFragment.java
@@ -3,6 +3,7 @@ package com.unithon7th.unithon_nb;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,9 +46,10 @@ public class AgriculturalRegisterFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_agricultural_register, container, false);
+
 
         View selectMiniLayout = view.findViewById(R.id.select_mini_classification);
 
--- unithon_nb/app/src/main/java/com/unithon7th/unithon_nb/LivestockRegisterFragment.java
+++ unithon_nb/app/src/main/java/com/unithon7th/unithon_nb/LivestockRegisterFragment.java
@@ -3,10 +3,17 @@ package com.unithon7th.unithon_nb;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bigkoo.pickerview.MyOptionsPickerView;
+
+import java.util.ArrayList;
 
 
 public class LivestockRegisterFragment extends Fragment {
@@ -40,11 +47,96 @@ public class LivestockRegisterFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_livestock_register, container, false);
 
+        final View farmingKindNameLayout = view.findViewById(R.id.livestock_00);
+
+        TextView subSelect = farmingKindNameLayout.findViewById(R.id.tv_sub_select);
+        subSelect.setText("마리 수");
+        TextView subSelectAnswer = farmingKindNameLayout.findViewById(R.id.tv_sub_selected_answer);
+        subSelectAnswer.setText("마리");
+
+        final View currentConditionLayout = view.findViewById(R.id.livestock_01);
+
+        TextView currentConditionLayout_subSelect = currentConditionLayout.findViewById(R.id.tv_sub_select);
+        currentConditionLayout_subSelect.setText("총 마리중");
+        TextView currentConditionLayout_subSelectAnswer = currentConditionLayout.findViewById(R.id.tv_sub_selected_answer);
+        currentConditionLayout_subSelectAnswer.setText("암,수 선택하기");
+
+        final View selectCountLayout = view.findViewById(R.id.livestock_02);
+        TextView selectCountLayout_subSelect = selectCountLayout.findViewById(R.id.tv_sub_select);
+        selectCountLayout_subSelect.setText("총 마리중");
+        TextView selectCountLayout_subSelectAnswer = selectCountLayout.findViewById(R.id.tv_sub_selected_answer);
+        selectCountLayout_subSelectAnswer.setText("암,수 선택하기");
+
+        farmingKindNameLayout.setVisibility(View.VISIBLE);
+        currentConditionLayout.setVisibility(View.GONE);
+        selectCountLayout.setVisibility(View.GONE);
+
+        farmingKindNameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                singlePicker(currentConditionLayout);
+            }
+        });
+
+        currentConditionLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                singlePicker(selectCountLayout);
+            }
+        });
+
+        selectCountLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                singlePicker();
+            }
+        });
         return view;
+    }
+
+    private void singlePicker(final View view) {
+        MyOptionsPickerView<String> singlePicker = new MyOptionsPickerView<String>(getActivity());
+        final ArrayList<String> items = new ArrayList<String>();
+        items.add("A");
+        items.add("B");
+        items.add("C");
+        items.add("D");
+        items.add("E");
+        singlePicker.setPicker(items);
+        singlePicker.setCyclic(false);
+        singlePicker.setSelectOptions(0);
+        singlePicker.setOnoptionsSelectListener(new MyOptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3) {
+                Toast.makeText(getActivity(), "" + items.get(options1), Toast.LENGTH_SHORT).show();
+                view.setVisibility(View.VISIBLE);
+            }
+        });
+        singlePicker.show();
+    }
+
+    private void singlePicker() {
+        MyOptionsPickerView<String> singlePicker = new MyOptionsPickerView<String>(getActivity());
+        final ArrayList<String> items = new ArrayList<String>();
+        items.add("A");
+        items.add("B");
+        items.add("C");
+        items.add("D");
+        items.add("E");
+        singlePicker.setPicker(items);
+        singlePicker.setCyclic(false);
+        singlePicker.setSelectOptions(0);
+        singlePicker.setOnoptionsSelectListener(new MyOptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3) {
+                Toast.makeText(getActivity(), "" + items.get(options1), Toast.LENGTH_SHORT).show();
+            }
+        });
+        singlePicker.show();
     }
 
     public void onButtonPressed(Uri uri) {
